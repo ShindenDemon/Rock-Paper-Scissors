ea5cc325748c5c4fafdcbd20e2bd8b2227ce7256
ShindevDan <damadu200@gmail.com>
2025-03-24 11:25:38 -0500

    Made some changes in Kris handling method and started the method for handling Quincy's play

--- RPS.py
+++ RPS.py
@@ -1,25 +1,38 @@
 import random
+from collections import Counter
 
 def player(prev_play, opponent_history=[]):
     # Variable for the first move
     guess = "P"
+    # Dictionary with counterplays
     counter_play = {"P": "S", "S": "R", "R": "P"}
 
     # Saving opponent moves
     if prev_play:
         opponent_history.append(prev_play)  
     else:
-        counter = 0 # Variable to time the counter
-        return guess  # First move 
+        return guess    # First move 
 
-    # Handle Kris
+    # Handling Kris
     if len(opponent_history) == 1:        
-        return guess
+        return guess    # Make the same move for the second round
     elif len(opponent_history) == 2:
-        return counter_play[prev_play]    
+        return counter_play[prev_play]  # Counterplay in the third round, necessary for handling Kris    
     else:     
-        print(len(opponent_history))           
-        return prev_play
+        return prev_play    # Trap Kris in a losing loop
+
+    # Handling Quincy
+    # As Quincy do a fixed five length pattern, opponent_history must have at least five values for Quincy's pattern checking
+    if len(opponent_history) >=6:
+        last_five = opponent_history[-5:]   # Last five opponent moves
+        count = Counter(last_five)  # Count values of R,P,S in the last five opponent moves in order to detect Quincy's pattern
+
+    # As pattern is ["R","R","P","P",S] if count have R = 2, P = 2 and S = 1, it may be Quincy's play
+        
+        ''' Write code to detect the pattern, identify which part of the sequence is being executed, 
+            and then apply countermeasures 
+        '''
+    
         
         
     
