d10fd3418e839f6996e6405d96486e4e63d567a3
ShindevDan <damadu200@gmail.com>
2025-04-23 11:04:07 -0500

    Created a dictionary with all Quincy´s possible patterns, then check if the opponent last five plays are in one of those patterns. This is for Quincy´s detection

--- RPS.py
+++ RPS.py
@@ -25,12 +25,19 @@ def player(prev_play, opponent_history=[]):
     # As Quincy do a fixed five length pattern, opponent_history must have at least five values for Quincy's pattern checking
     if len(opponent_history) >=6:
         last_five = opponent_history[-5:]   # Last five opponent moves
-        count = Counter(last_five)  # Count values of R,P,S in the last five opponent moves in order to detect Quincy's pattern
+        Quincy_patterns = {1:['R','R','P','P','S'],
+                           2:['R','P','P','S','R'],        
+                           3:['P','P','S','R','R'],     # Possible Quincy´s patterns     
+                           4:['P','S','R','R','P'], 
+                           5:['S','R','R','P','P']}
 
-    # As pattern is ["R","R","P","P",S] if count have R = 2, P = 2 and S = 1, it may be Quincy's play
+        for pattern in Quincy_patterns.values():    
+            if last_five == pattern:    # Check if Quincy´s pattern is being executed    
+                is_Quincy == True       # Identifying Quincy as opponent 
         
-        ''' Write code to detect the pattern, identify which part of the sequence is being executed, 
-            and then apply countermeasures 
+        ''' Identify which part of the sequence is being executed, 
+            and then apply countermeasures. Maybe using some variables (is_xxxx)
+            would help bot handling easily.
         '''
     
         
